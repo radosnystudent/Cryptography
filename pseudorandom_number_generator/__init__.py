cf27c9ea9b9101ec3769123c7636864c5ac5579f
radosnystudent <jeremiasz.pauszek@gmail.com>
2020-11-17 13:48:29 +0100

    better PRNG

--- pseudorandom_number_generator/__init__.py
+++ pseudorandom_number_generator/__init__.py
@@ -3,39 +3,31 @@ Zaimplementuj algorytm, który generuje losowy element zbioru Zn.
 Dane: k,n ∈ N
 Wynik: k-bitowa liczba b ∈ Zn
 """
-
+from matplotlib import pyplot as plt
 import datetime as dt
+from collections import Counter
+from modular_exponentiation import powMod
 
-GENERATOR: int = 2057
-MODULUS: int = 3167
-
-def L_function(x: int) -> int:
-    # any polynomial function
-    return 3 * x ** 2 - 5 * x + 9
-
-def F_function(x: str):
-    # f(x) <- g^x mod p
-    return "{0:b}".format(pow(GENERATOR, int(x, 2), MODULUS))
+GENERATOR: int = 23
+MODULUS: int = 36389
 
 def HFunction(first_half: str, second_half: str) -> str:
 
     # one-way permutation function
     # function H (x, y)
-    #   f(x) <- g^x mod p  where g is a generator
+    #   f(x) <- g^x mod p  where g is a generator and p is prime number
     #   b <- 0
     #   for i <- 0 to |x| do
     #       b <- b XOR (x[i] AND y[i])
     #   return f + y + b
 
-    fx: str = F_function(first_half)
+    fx: str = "{0:b}".format(powMod(GENERATOR, int(first_half, 2), MODULUS))
     hard_core_bit: int = 0
     for i in range(len(first_half)):
         hard_core_bit = (hard_core_bit ^ (int(first_half[i]) & int(second_half[i]))) % 2
     return fx + second_half + str(hard_core_bit)
 
 def PRNG(initial_seed: str, n: int) -> str:
-
-    SEED_SIZE: int = len(initial_seed)
 
     # pseudorandom number generator
     # function G(x0)
@@ -49,15 +41,18 @@ def PRNG(initial_seed: str, n: int) -> str:
 
     # because we need k-bit numbers form range [0, n-1], we need to check if generated number is in given range
     # if not, generate another
+    iteration = 0
     while True:
         result: str = '1'
-        binary_string: str = "{0:b}".format(int(dt.datetime.now().timestamp() * 10000))
-        for _ in range(SEED_SIZE - 1):
+        binary_string: str = "{0:b}".format(int(dt.datetime.now().timestamp() * 100000000) + iteration)
+        # print(f'{int(dt.datetime.now().timestamp() * 10000)} : {binary_string}')
+        for _ in range(len(initial_seed) - 1):
 
             first_half: str = binary_string[:int(len(binary_string) / 2)]
             second_half: str = binary_string[int(len(binary_string) / 2):]
 
             binary_string = HFunction(first_half, second_half)
+            # print(binary_string)
             result += binary_string[-1]
 
             binary_string = binary_string[:-1]
@@ -76,15 +71,24 @@ def main():
 
     # range [min, max] <- all k-bit numbers
     minValue: int = int('1' + '0' * (SEED_SIZE - 1), 2)
-    maxValue: int = int("{0:b}".format(n), 2)
+    maxValue: int = int("{0:b}".format(n), 2) - 1 if int("{0:b}".format(n), 2) <= int('1' * (SEED_SIZE), 2) else int('1' * (SEED_SIZE), 2)
+
+    # temp1 = int("{0:b}".format(n), 2)
+    # temp2 = int('1' * (SEED_SIZE), 2)
+
+    # print(f'{temp1} vs {temp2} : {temp1 <= temp2}')
 
     if n > minValue:
         print(f'początkowa liczba: {int(inputValue, 2)}; liczba bitów: {SEED_SIZE}')
-        print(f'Przedział liczb: [{minValue}, {maxValue})')
+        print(f'Przedział liczb: [{minValue}, {maxValue}]')
         results: list = list()
+
         for _ in range(N):
-            results.append(int(PRNG(inputValue, n), 2))
-            MODULUS = abs(MODULUS - L_function(SEED_SIZE))
+            inputValue = PRNG(inputValue, n)
+            results.append(int(inputValue, 2))
         print(f'results: {results}')
+        # results.sort()
+        plt.bar(*zip(*Counter(results).items()))
+        plt.show()
     else:
         print(f'nie ma {SEED_SIZE}-bitowych liczb, które byłyby w przedziale [0, {n - 1}]')
